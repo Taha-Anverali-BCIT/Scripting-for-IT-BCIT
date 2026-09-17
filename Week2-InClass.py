0d96688e2b14eb087d790cb67353a1baf7636cb0
.\.gitTaha Anverali <tahaanverali30@gmail.com>
2026-09-17 15:39:46 -0700

    Week2-InClass.py

---
+++ Week2-InClass.py
@@ -0,0 +1,37 @@
+# Create a variable named x and store an integer (whole number) inside it
+x = 20
+
+# Create a variable named y and store a string (any characters between single or double quotes) inside it
+y = "My name is Taha"
+
+# Create a variable named z and store a float in it
+z = 3.14
+
+# Change the value stored in the x variable to a new string
+x = "I am 20 years old"
+
+# Change the value stored in the y variable to a new boolean
+y = True
+
+# Change the value store in the z variable to a different float
+z = 2.71
+
+# Print the value the user entered from the previous section
+input_value = input("Please enter a value: ")
+print(input_value)
+
+# Print the (current) value of the variable x
+print(x)
+
+# Print the *type* (not the value itself) of the value stored in the y variable
+print(type(y))
+
+# Create two variables, one containing the string CIT, and another containing the string 1515
+c = "CIT"
+n = "1515"
+
+# Using the two variables and a hard-coded letter, print the word ACIT1515 to the terminal
+print(f"A{c}{n}")
+
+assignment_number = 2
+print(f'Assignment {assignment_number} created')
